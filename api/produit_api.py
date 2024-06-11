8f4c7bea52026f466dff17fa21bee5f1345677fb
Thibaut <jobardthibaut12@gmail.com>
2024-06-11 21:59:04 +0200

    coverage des tests

--- api/produit_api.py
+++ api/produit_api.py
@@ -4,7 +4,7 @@ from typing import List
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
-import os
+
 
 # Création de l'instance FastAPI pour initialiser l'application et permettre la définition des routes.
 app = FastAPI()
